5141245cf3ec0acbd41eb25811375b7bced3f524
Manoj Bhumia <manoj.bhumia83@gmail.com>
2026-04-05 15:22:00 +0530

    Update Main.java

--- src/scheme/Main.java
+++ src/scheme/Main.java
@@ -1,43 +1,45 @@
 package scheme;
 
 import arc.scene.event.Touchable;
+import arc.util.Time;
+import mindustry.game.EventType.GameLoadEvent;
 import mindustry.gen.Icon;
 import mindustry.mod.Mod;
 import mindustry.ui.Styles;
+import arc.Events;
 import static arc.Core.*;
 import static mindustry.Vars.*;
 
 public class Main extends Mod {
 
-    public static LockDesktopInput desktopInput;
-    public static LockMobileInput mobileInput;
+    public static boolean locked = false;
 
-   @Override
-public void init() {
-    // set input FIRST, outside of any fill/lambda
-    if (mobile) {
-        mobileInput = new LockMobileInput();
-        control.setInput(mobileInput);
-    } else {
-        desktopInput = new LockDesktopInput();
-        control.setInput(desktopInput);
-    }
+    @Override
+    public void init() {
+        // hook into update loop without replacing input handler
+        Events.run(arc.Events.class, () -> {});  
+        
+        ui.hudGroup.fill(cont -> {
+            cont.name = "lockmovement";
+            cont.top().left();
+            cont.touchable = Touchable.childrenOnly;
+            cont.visible(() -> ui.hudfrag.shown && !ui.minimapfrag.shown());
+            cont.table(pad -> {
+                pad.button(Icon.lock, Styles.clearNoneTogglei, () -> locked = !locked)
+                   .checked(b -> locked)
+                   .size(46f)
+                   .padTop(mobile ? 69f : 0f);
+            });
+        });
 
-    // then build UI
-    ui.hudGroup.fill(cont -> {
-        cont.name = "lockmovement";
-        cont.top().left();
-        cont.touchable = Touchable.childrenOnly;
-        cont.visible(() -> ui.hudfrag.shown && !ui.minimapfrag.shown());
-        cont.table(pad -> {
-            if (mobile) {
-                pad.button(Icon.lock, Styles.clearNoneTogglei, () -> mobileInput.toggleLock())
-                   .checked(b -> mobileInput.locked).size(46f).padTop(69f);
-            } else {
-                pad.button(Icon.lock, Styles.clearNoneTogglei, () -> desktopInput.toggleLock())
-                   .checked(b -> desktopInput.locked).size(46f);
+        // intercept movement each frame via renderer
+        renderer.addEnvRenderer(0, () -> {
+            if (locked && player != null && player.unit() != null && !state.isMenu()) {
+                player.unit().vel.setZero();
+                player.vel.setZero();
+                player.unit().moveX = 0;
+                player.unit().moveY = 0;
             }
         });
-    });
-}
+    }
 }
